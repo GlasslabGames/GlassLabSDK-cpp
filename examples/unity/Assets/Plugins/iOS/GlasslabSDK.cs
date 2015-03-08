c4c36bef2cd8d884633a8ecd874f485a9e26b1a6
Ben Dapkiewicz <ben@glasslabgames.org>
2015-03-07 20:39:41 -0800

    updated the GlassLabSDK.cs for Windows support fixed bug with ios/osx 64 bit crash

--- examples/unity/Assets/Plugins/iOS/GlasslabSDK.cs
+++ examples/unity/Assets/Plugins/iOS/GlasslabSDK.cs
@@ -1,3 +1,35 @@
+/*
+
+Copyright (c) 2014, GlassLab, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+
+*/
+
+
 using UnityEngine;
 using System;
 using System.Collections;
@@ -1002,6 +1034,9 @@ public class GlasslabSDK {
 	 */
 	#if UNITY_IPHONE
 	[DllImport ("__Internal")]
+	private static extern void GlasslabSDK_SetGameSecret(System.IntPtr inst, string gameSecret);
+
+	[DllImport ("__Internal")]
 	private static extern void GlasslabSDK_SetName(System.IntPtr inst, string name);
 	
 	[DllImport ("__Internal")]
@@ -1026,6 +1061,9 @@ public class GlasslabSDK {
 	private static extern void GlasslabSDK_SetAutoSessionManagement(System.IntPtr inst, bool state);
 	#endif
 	#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+	[DllImport ("GlassLabSDK")]
+	private static extern void GlasslabSDK_SetGameSecret(System.IntPtr inst, string gameSecret);
+	
 	[DllImport ("GlassLabSDK")]
 	private static extern void GlasslabSDK_SetName(System.IntPtr inst, string name);
 	
